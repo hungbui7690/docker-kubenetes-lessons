8b0032bd49b44b1027a893141d312f8789111490
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 04:56:52 +0700

    Rebuilds with Cache

--- playground/notes.js
+++ playground/notes.js
@@ -1,5 +1,24 @@
 /*
-  A Brief Recap
+  Rebuilds with Cache
   - pic
 
+  - add new line in Dockerfile
+    > RUN apk add --update redis
+      RUN apk add --update gcc 
+
+  - rebuild
+    > docker build .
+
+////////////////////////////////////
+
+  - if we build again > gcc will use caching to install as well 
+
+////////////////////////////////////
+
+  - but if we switch the 2 RUN lines > now, it will not use caching anymore for both
+    > RUN apk add --update gcc
+      RUN apk add --update redis
+  
+
+  *** we know that when we change the Dockerfile, we cannot use caching >> put the change far down of the file as possible
 */
