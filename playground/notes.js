9ea33bccd5aa8951833691ea5c106f686237b0dd
hungbui7690 <hungbui7690@gmail.com>
2023-03-30 14:31:33 +0700

    What is Docker

--- playground/notes.js
+++ playground/notes.js
@@ -1,13 +1,5 @@
 /*
-  Why Use Docker
+  What is Docker
   - pic
-  - fix installation problems 
-    > with docker, it is so easy to install softwares & dependencies
-
-  - docker run -it redis 
-    > install + running instantly
-
-
-  *** Check commands.js for all commands in the course
-
+  
 */
