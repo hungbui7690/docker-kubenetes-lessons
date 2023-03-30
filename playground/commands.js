fd3244456f8efadb6d8a8a46c6985b373171feee
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 05:49:18 +0700

    Project Outline

--- playground/commands.js
+++ playground/commands.js
@@ -1,10 +1,91 @@
 /*
 
   docker version
+  
+  Docker Run: 
+  - docker run -it redis 
+  - docker run hello-world
+  - docker run <image_name>   
+  - docker run = docker create + docker start
 
-  docker run -it redis 
+  Override: 
+  - docker run busybox echo hi there
+  - docker run busybox ls
+  - docker run hello-world ls
+  - docker run hello-world echo hi there
 
-  docker run hello-world
+  List out containers
+  - docker ps  
+  - docker ps --all
+
+  Life Cycle
+  - docker create hello-world
+  - docker start <id> 
+  - docker start -a <id>
+
+  Remove Stopped Containers
+  - docker system prune
+
+  Retrieving Log Outputs
+  - docker logs <id>
+
+  Stopping Containers
+  - docker stop <id>
+  - docker kill <id>
+
+  Execute Command in "Running" Container
+  - docker exec -it <id> <command>
+
+  - Shell
+    > docker exec -it <id> sh
+
+  Run a Container with Shell 
+  - docker run -it <image_name> <command>
+    > docker run -it busybox sh
+
+  *** this will not start any process in that image
+
+/////////////////////////////////////////
+// IMAGES
+/////////////////////////////////////////
+
+  - docker build .
+
+  - build with tags: 
+    > docker build -t hungbui7690/redis:latest .
+    > docker run hungbui7690/redis
+
+
+
+
+
+
+
+/////////////////////////////////////////
+// Dockerfile
+/////////////////////////////////////////
+
+# Use an existing docker image as a base
+FROM alpine 
+
+# Download an install a dependency
+RUN apk add --update redis
+RUN apk add --update gcc
+
+# Tell the image what to do when it starts as a container
+CMD ["redis-server"]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- playground/notes.js
+++ playground/notes.js
@@ -1,4 +1,7 @@
 /*
-  
+  Project Outline
+  - pic
+  - create nodejs app and put in container 
+  - after that, we can connect from the outside  
 
 */
