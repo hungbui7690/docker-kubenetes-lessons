ef829dc1ba6f512e0e79701976f4d2dac728c6ec
hungbui7690 <hungbui7690@gmail.com>
2023-03-30 14:29:53 +0700

    Why Use Docker

---
+++ playground/notes.js
@@ -0,0 +1,13 @@
+/*
+  Why Use Docker
+  - pic
+  - fix installation problems 
+    > with docker, it is so easy to install softwares & dependencies
+
+  - docker run -it redis 
+    > install + running instantly
+
+
+  *** Check commands.js for all commands in the course
+
+*/
