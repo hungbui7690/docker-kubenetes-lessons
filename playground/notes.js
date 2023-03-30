b710a83e2810559b2f81677e7df31ea5c640f81f
hungbui7690 <hungbui7690@gmail.com>
2023-03-30 14:56:18 +0700

    But Really...What's a Container

--- playground/notes.js
+++ playground/notes.js
@@ -1,8 +1,7 @@
 /*
-  Using the Docker Client
+  But Really...What's a Container
   - pic
 
-  - docker run hello-world
-
+  - Kernel: running software process that uses to communicate between softwares on our computer & hardwares (CPU, Memory, Hard Disk...)
 
 */
