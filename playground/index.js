27f780324fdc190469304abfafdc926c73eff04b
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 06:59:57 +0700

    Base Image Issues P1

--- playground/index.js
+++ playground/index.js
@@ -1,22 +1,14 @@
 /*
-  Required WORKDIR update - Could not detect node name, idealTree, errors
-  - 10-24-2020
+  Base Image Issues P1
+  - we got the error in prev lesson > because we don't have npm in the base image (alpine)
+  - to fix, 2 options: 
+    + find another base image that has node and npm pre-installed inside it
+    + install ourself
 
-  - The v15 version of Node has recently been released and is causing issues with some of our project code.
-
-  - In the next lecture you may get the following error when building the Dockerfile:
-    > npm ERR! could not detect node name from path or package
-    or
-    > npm ERR! idealTree already exists
-
-  - To resolve this, add a WORKDIR right after the FROM instruction: (we will be adding this very soon anyway)
-      FROM node:alpine
-
-      WORKDIR /usr/app
-
-  - Also, you will no longer get the error regarding the missing package.json as shown at around 7:16 in the video. With Node v15 / npm v7, if you run npm install in a directory without a package.json, an empty package.json is now created for you and will no longer throw this error. However, a properly written package.json listing your dependencies is still required to run a Node based application.
-
-  
+  - pic > we want to use version 6.14
+  - Dockerfile
+    > FROM node:6.14 
+    
 */
 
 const express = require('express')
