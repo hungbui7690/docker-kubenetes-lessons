85da0949670a41d240dc82aca14f615ae3dc5182
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 05:59:35 +0700

    Required WORKDIR update

--- playground/index.js
+++ playground/index.js
@@ -1,18 +1,22 @@
 /*
-  A Few Planned Errors
-  - now, we want to wrap this project into a container 
-  - to start nodejs app:
-    + npm install 
-    + npm start
+  Required WORKDIR update - Could not detect node name, idealTree, errors
+  - 10-24-2020
 
-  - pic
+  - The v15 version of Node has recently been released and is causing issues with some of our project code.
 
-  (1)
-  - create Dockerfile
+  - In the next lecture you may get the following error when building the Dockerfile:
+    > npm ERR! could not detect node name from path or package
+    or
+    > npm ERR! idealTree already exists
 
-  (2)
-  - docker build .
-    > error: npm: not found
+  - To resolve this, add a WORKDIR right after the FROM instruction: (we will be adding this very soon anyway)
+      FROM node:alpine
+
+      WORKDIR /usr/app
+
+  - Also, you will no longer get the error regarding the missing package.json as shown at around 7:16 in the video. With Node v15 / npm v7, if you run npm install in a directory without a package.json, an empty package.json is now created for you and will no longer throw this error. However, a properly written package.json listing your dependencies is still required to run a Node based application.
+
+  
 */
 
 const express = require('express')
