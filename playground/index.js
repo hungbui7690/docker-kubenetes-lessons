d5bc800058d6f338f8b5b003ce9b53d288065b3b
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 08:35:29 +0700

    App Server Starter Code

---
+++ playground/index.js
@@ -0,0 +1,27 @@
+/*
+  Assembling a Dockerfile
+  - create Dockerfile
+
+  > docker build -t hungbui7690/visits:latest .
+
+*/
+
+const express = require('express')
+const redis = require('redis') // ***
+
+const app = express()
+
+// *** initialize
+const client = redis.createClient()
+client.set('visits', 0)
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is: ' + visits)
+    client.set('visits', parseInt(visits) + 1)
+  })
+})
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081... ')
+})
