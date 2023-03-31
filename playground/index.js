4bc3e15f368db3d621a576ca0bd4565d146f1137
hungbui7690 <hungbui7690@gmail.com>
2023-03-31 09:32:36 +0700

    Introducing Docker Compose

--- playground/index.js
+++ playground/index.js
@@ -1,17 +1,29 @@
 /*
-  Assembling a Dockerfile
-  - create Dockerfile
+  Introducing Docker Compose
+  - docker run hungbui7690/visits
+    > error: connect ECONNREFUSED 127.0.0.1:6379
+    > our server is stating up, but there's no redis server to connect to 
 
-  > docker build -t hungbui7690/visits:latest .
+  - so we need to have redis server
+    > docker run redis
+
+  - after this setup, still have the same error 
+
+  - pic
+
+  - run: docker-compose
+    > show all options
+
+  *** docker-compose helps us do not make repetitive commands in docker-cli
+    > helps us to run multiple docker commands quickly 
 
 */
 
 const express = require('express')
-const redis = require('redis') // ***
+const redis = require('redis')
 
 const app = express()
 
-// *** initialize
 const client = redis.createClient()
 client.set('visits', 0)
 
