38cc3f2b2527c6f8acfdac64ba666639db3dab34
hungbui7690 <hungbui7690@gmail.com>
2023-04-01 02:37:27 +0700

    Services Overview

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,19 +1,6 @@
 /*
-  Running Nginx
-  - docker build .
-    > <image_id>
-
-  - docker run -p 8080:80 <image_id>
-    > need to have port
-      > default port of nginx is 80
-
-
-
-  *** Dockerfile
-    > COPY --from=builder /app/build /usr/share/nginx/html
-
-    CHANGE TO: --from=0
-    > COPY --from=0 /app/build /usr/share/nginx/html
+  Services Overview
+  - pic
 
 */
 
