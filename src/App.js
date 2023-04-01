a14b15a1a58e1a26c0ddd2d1cc1de16111fc08d3
hungbui7690 <hungbui7690@gmail.com>
2023-04-01 11:48:34 +0700

    AWS Elastic Beanstalk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 /*
-  Automatic Build Creation P2
-  - to fix the error in previous lesson 
-    + By default npm test runs the watcher with interactive CLI. However, you can force it to run tests once and finish the process by setting an environment variable called CI.
-      > -e CI=true
-
-  
-  - Add -e CI= true to the script in .travis.yml
-      script:
-      - docker run -e CI=true hungbui7690/docker-react npm run test -- --coverage
-
-  - pic: result in Travis CI
-
-////////////////////////////////////////////
-  ***
-  - You can read up on the CI=true variable here:
-      https://facebook.github.io/create-react-app/docs/running-tests#linux-macos-bash
-
-  - and environment variables in Docker here:
-      https://docs.docker.com/engine/reference/run/#env-environment-variables
-
+  AWS Elastic Beanstalk
+  - pic
 
 */
 
