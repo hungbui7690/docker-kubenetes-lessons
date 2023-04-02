aadbab7d59fbf5cef2b3681eb08b8bd3c4f8a014
hungbui7690 <hungbui7690@gmail.com>
2023-04-02 07:28:27 +0700

    Workflow With Github

--- src/App.js
+++ src/App.js
@@ -1,55 +1,6 @@
 /*
-  Exposing Ports Through the Dockerfile *******
+  Workflow With Github
   - pic
-
-  - Dockerfile
-      EXPOSE 80
-      > when working locally, this line is just to tell other devs that we use port 80 for this service 
-      > but when it comes to AWS > AWS need this to map the port 
-
-
-  - docker-compose.yml
-    > check below
-
-  - push again
-
-****************************************
-  *** BAD GATEWAY 502 
-****************************************
-    In case we cannot deploy to AWS many times: 
-    > Try to Rebuild the Environment
-
-    NUCLEAR OPTION
-    > Delete AWS Beanstalk Environment + Application 
-    > Recreate
-    > Deploy Again
-
-****************************************
-
-  This new AWS platform will conflict with the project we have built since it will look for a docker.compose.yml file to build from by default instead of a Dockerfile.
-
-  To resolve this, please do the following:
-
-    1. Rename the development Compose config file
-
-      Rename the docker-compose.yml file to docker-compose-dev.yml. Going forward you will need to pass a flag to specify which compose file you want to build and run from:
-        docker-compose -f docker-compose-dev.yml up
-        docker-compose -f docker-compose-dev.yml up --build
-        docker-compose -f docker-compose-dev.yml down
-
-    2. Create a production Compose config file
-
-      Create a docker-compose.yml file in the root of the project and paste the following:
-        version: '3'
-        services:
-          web:
-            build:
-              context: .
-              dockerfile: Dockerfile
-            ports:
-              - '80:80'
-      AWS EBS will see a file named docker-compose.yml and use it to build the single container application.
-
 
 */
 
